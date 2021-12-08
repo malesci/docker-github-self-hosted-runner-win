d5f353db97bfd2b8f4fa85492172d0e153a4d2ca
Mariano <36844281+malesci@users.noreply.github.com>
2021-12-08 16:41:11 +0100

    fix test Choco

--- image/Tests/ChocoPackages.Tests.ps1
+++ image/Tests/ChocoPackages.Tests.ps1
@@ -1,30 +1,30 @@
 Describe "7-Zip" {
     It "7z" {
-        "7z" | Should -ReturnZeroExitCode
+        7z | Should -ReturnZeroExitCode
     }
 }
 
 Describe "Aria2" {
     It "Aria2" {
-        "aria2c --version" | Should -ReturnZeroExitCode
+        aria2c --version | Should -ReturnZeroExitCode
     }
 }
 
 Describe "AzCopy" {
     It "AzCopy" {
-        "azcopy --version" | Should -ReturnZeroExitCode
+        azcopy --version | Should -ReturnZeroExitCode
     }
 }
 
 Describe "Bicep" {
     It "Bicep" {
-        "bicep --version" | Should -ReturnZeroExitCode
+        bicep --version  | Should -ReturnZeroExitCode
     }
 }
 
 Describe "GitVersion" -Skip:(Test-IsWin22) {
     It "gitversion is installed" {
-        "gitversion /version" | Should -ReturnZeroExitCode
+        gitversion /version  | Should -ReturnZeroExitCode
     }
 }
 
@@ -36,70 +36,70 @@ Describe "InnoSetup" -Skip:(Test-IsWin22) {
 
 Describe "Jq" {
     It "Jq" {
-        "jq -n ." | Should -ReturnZeroExitCode
+        jq -n .  | Should -ReturnZeroExitCode
     }
 }
 
 Describe "Nuget" {
     It "Nuget" {
-       "nuget" | Should -ReturnZeroExitCode
+        nuget  | Should -ReturnZeroExitCode
     }
 }
 
 Describe "OpenSSL" {
     It "OpenSSL" {
-       "openssl version" | Should -ReturnZeroExitCode
+        openssl version  | Should -ReturnZeroExitCode
     }
 }
 
 Describe "Packer" {
     It "Packer" {
-       "packer --version" | Should -ReturnZeroExitCode
+        packer --version  | Should -ReturnZeroExitCode
     }
 }
 
 Describe "Perl" -Skip:(Test-IsWin22) {
     It "Perl" {
-       "perl --version" | Should -ReturnZeroExitCode
+        perl --version  | Should -ReturnZeroExitCode
     }
 }
 
 Describe "Pulumi" {
     It "pulumi" {
-       "pulumi version" | Should -ReturnZeroExitCode
+        pulumi version  | Should -ReturnZeroExitCode
     }
 }
 
 Describe "Svn" {
     It "svn" {
-        "svn --version --quiet" | Should -ReturnZeroExitCode
+        svn --version --quiet  | Should -ReturnZeroExitCode
     }
 }
 
 Describe "Swig" {
     It "Swig" {
-        "swig -version" | Should -ReturnZeroExitCode
+        swig -version  | Should -ReturnZeroExitCode
     }
 }
 
 Describe "VSWhere" {
     It "vswhere" {
-        "vswhere" | Should -ReturnZeroExitCode
+        vswhere  | Should -ReturnZeroExitCode
     }
 }
 
 Describe "Julia" {
     It "Julia path exists" {
-        "C:\Julia" | Should -Exist
+        C:\Julia | Should -Exist
     }
 
     It "Julia" {
-        "julia --version" | Should -ReturnZeroExitCode
+        julia --version  | Should -ReturnZeroExitCode
     }
 }
 
 Describe "CMake" {
     It "cmake" {
-        "cmake --version" | Should -ReturnZeroExitCode
+        cmake --version  | Should -ReturnZeroExitCode
     }
 }
